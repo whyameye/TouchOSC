b19a8936a039b04da8fecfb63316ec5b7a7b31bd
John <john.harrison@alum.mit.edu>
2025-02-19 08:24:08 -0600

    first commit

---
+++ TouchOSC/TouchOSC/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using libSMARTMultiTouch.Table;
+
+namespace Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TableManager.Initialize(this, LayoutRoot);
+            TableControl tableControl = new TableControl();
+            tableControl.Opacity = .01;
+            LayoutRoot.Children.Add(tableControl);
+            // this.Cursor = Cursors.None;
+            //TableManager.IsFullScreen = false;
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ TouchOSC/TouchOSC/TableControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿//main program for app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using libSMARTMultiTouch.Controls;
+using Main;
+
+namespace Main
+{
+    /// <summary>
+    /// Interaction logic for TableControl.xaml
+    /// </summary>
+    public partial class TableControl : TableApplicationControl
+    {
+
+        public TableControl()
+        {
+            InitializeComponent();
+        }
+
+        private void TableApplicationControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            MainCanvas mapCanvas = new MainCanvas();
+            TableLayoutRoot.Children.Add(mapCanvas);
+        }
+    }
+}
