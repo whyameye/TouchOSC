c9b1da2e5b012db3a70368acc29e2d5d67f056d7
John <john.harrison@alum.mit.edu>
2025-03-08 16:33:33 -0600

    OSC added (untested)

--- TouchOSC/TouchOSC/MainCanvas.cs
+++ TouchOSC/TouchOSC/MainCanvas.cs
@@ -21,6 +21,13 @@ namespace Main
 {
     class MainCanvas : Canvas
     {
+        private static int frameCount = 0;
+        private static int[] blobIDs = new int[40]; // defaults to initial value of 0
+        private static double[] blobXs = new double[40];
+        private static double[] blobYs = new double[40];
+
+        private static SharpOSC.UDPSender oscSender = new SharpOSC.UDPSender("127.0.0.1", 3333);
+
         public MainCanvas()
         {
             this.Width = SystemParameters.PrimaryScreenWidth;
@@ -31,22 +38,76 @@ namespace Main
             b.TouchDown += new TouchContactEventHandler(TouchAreaTouchDown);
             b.TouchUp += new TouchContactEventHandler(TouchAreaTouchUp);
             b.TouchMove += new TouchContactEventHandler(TouchAreaTouchMove);
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+            Debug.WriteLine("Main Canvas Constructor Complete");
+        }
+
+        private void CompositionTarget_Rendering(object sender, EventArgs e)
+        {
+            // send blob coordinates
+            int i = 0;
+            List<object> blobAlives = new List<object>();
+            blobAlives.Add("alive");
+            while (blobIDs[i] != 0)
+            {
+                var message2 = new SharpOSC.OscMessage("/tuio/2Dcur", "set", blobIDs[i], (float) blobXs[i], (float) blobYs[i]);
+                oscSender.Send(message2);
+                blobAlives.Add(blobIDs[i]);
+                i++;
+            }
+
+            // send blob IDs that are alive
+            object[] blobAliveArray = blobAlives.ToArray();
+            var message = new SharpOSC.OscMessage("/tuio/2Dcur", blobAliveArray);
+            oscSender.Send(message);
+
+            // send frame #
+            message = new SharpOSC.OscMessage("/tuio/2Dcur", "fseq",frameCount);
+            oscSender.Send(message);
+            frameCount++;
         }
 
         void TouchAreaTouchDown(object sender, libSMARTMultiTouch.Input.TouchContactEventArgs e)
         {
             TouchArea b = (TouchArea)sender;
             Debug.WriteLine("DOWN id: " + e.TouchContact.ID + "X: " + e.TouchContact.Position.X + " Y: " + e.TouchContact.Position.Y);
+            int i = 0;
+            while (blobIDs[i] != 0)
+                i++;
+            blobIDs[i] = e.TouchContact.ID;
+            blobXs[i] = e.TouchContact.Position.X / 1920;
+            blobYs[i] = e.TouchContact.Position.Y / 1080;
         }
 
         void TouchAreaTouchMove(object sender, TouchContactEventArgs e)
         {
-            //Debug.WriteLine("MOVE id: " + e.TouchContact.ID + "X: " + e.TouchContact.Position.X + " Y: " + e.TouchContact.Position.Y);
+            int i = 0;
+            while (blobIDs[i] != e.TouchContact.ID)
+                i++;
+            blobIDs[i] = e.TouchContact.ID;
+            blobXs[i] = e.TouchContact.Position.X / 1920;
+            blobYs[i] = e.TouchContact.Position.Y / 1080;
         }
 
         void TouchAreaTouchUp(object sender, libSMARTMultiTouch.Input.TouchContactEventArgs e)
         {
             Debug.WriteLine("UP id: " + e.TouchContact.ID + "X: " + e.TouchContact.Position.X + " Y: " + e.TouchContact.Position.Y);
+            int i = 0;
+            while (blobIDs[i] != e.TouchContact.ID)
+                i++;
+            int j = i + 1;
+            while (blobIDs[j] != 0)
+                j++;
+            j--;
+            if (j == i)
+                blobIDs[i] = 0;
+            else
+            {
+                blobIDs[i] = blobIDs[j];
+                blobXs[i] = blobXs[j];
+                blobYs[i] = blobYs[j];
+                blobIDs[j] = 0;
+            }
         }
     }
 }
